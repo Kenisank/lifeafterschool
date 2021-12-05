ce313d4f1aa79d2c17716c8fc368e7756141dd04
Kenechukwu Frank <kenisank1@gmail.com>
2021-12-05 13:48:25 +0100

    first

---
+++ Models/Attendees.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LifeAfterSchool.Models
+{
+    public class Attendees
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+
+        public string Department { get; set; }
+
+       
+        [Required(ErrorMessage = "You must provide a phone number")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+        //[Required(ErrorMessage = "Required")]
+        //[RegularExpression(@"^(\d{10})$", ErrorMessage = "Wrong mobile")]
+        public string PhoneNo { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "The email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public  string Email { get; set; }
+    }
+}
